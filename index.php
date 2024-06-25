89a38309dd54328ba3fcdd330670b1f8105644d0
Cristian Cruz <ccruzs1900@alumno.ipn.mx>
2024-06-25 00:20:21 -0600

    css del index

--- index.php
+++ index.php
@@ -20,26 +20,52 @@ $_SESSION['from_index'] = true;
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script defer src="script.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> <!-- Agrega esta línea -->
+    
 </head>
 <body>
-    <header>
-        <h1>Tienda en Línea</h1>
-        <nav>
-            <a href="carrito.php" class="btn btn-primary">Mi Carrito</a>
-            <a href="logout.php" class="btn btn-warning">Cerrar Sesión</a>
-        </nav>
-    </header>
-    <main id="product-list">
-        <div id="modal" class="modal">
-            <div class="modal-content">
-                <span class="close-button">&times;</span>
-                <p id="modal-text"></p>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" >Tiendita</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active text-danger" aria-current="page" href="logout.php">Cerrar sesión</a></li>
+                        
+                    </ul>
+                    <form class="d-flex">
+                    <a href="carrito.php" class="btn btn-outline-dark" type="submit">
+                            <i class="bi-cart-fill me-1"></i>
+                            Carrito
+                            
+                           
+                            </a>
+                    </form>
+                </div>
             </div>
-        </div>
-        <!-- Los productos se cargarán aquí dinámicamente -->
-    </main>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">Tiendita</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">Tiendita con variedad de productos</p>
+                </div>
+            </div>
+       
+        
+    </header>
+    <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center" id="product-list">
+                </div>
+            </div>
+        </section>               
     <script>
     var idCliente = "<?php echo $_SESSION['idCliente']; ?>";
     </script>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
